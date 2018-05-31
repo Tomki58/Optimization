54768a682aab9fa1bef164be74c2526e707bc4f0
Tomki58 <tomki589@gmail.com>
2018-05-31 18:13:28 +0300

    Без ранга матрицы, но шаг int

--- SopDir.cpp
+++ SopDir.cpp
@@ -1,4 +1,4 @@
-// SopDir.cpp: РѕРїСЂРµРґРµР»СЏРµС‚ С‚РѕС‡РєСѓ РІС…РѕРґР° РґР»СЏ РєРѕРЅСЃРѕР»СЊРЅРѕРіРѕ РїСЂРёР»РѕР¶РµРЅРёСЏ.
+// SopDir.cpp: определяет точку входа для консольного приложения.
 //
 
 #include "stdafx.h"
@@ -17,7 +17,7 @@ double f(double x0, double x1)
 	return 4 * pow((x0 - 5), 2) + pow((x1 - 6), 2);
 }
 
-double Optim(double x0, double x1, vector<double> dir)
+int Optim(double x0, double x1, vector<double> dir)
 {
 	int k = 0;
 	double a = -100; double b = 70;
@@ -34,37 +34,61 @@ double Optim(double x0, double x1, vector<double> dir)
 	return (a + b) / 2;
 }
 
-void Directions()
+int Directions()
 {
-	double x0 = 8; double x1 = 9;
-	int i = 0; int k = 0; int n = 2;
-	double y0 = NULL, y1 = NULL;
-	vector<double> p1 = { 1 , 0 };
-	vector<double> p2 = { 0, 1 };
+	vector<vector<double>> x = { {8,9} };
+	int i = 0; int k = 0; int n = 2;			// i = 0, k = 0
+	vector<double> p1 = { 1 , 0 };				// первое направление поиска
+	vector<double> p2 = { 0, 1 };				// второе направление поиска
 	vector<double> arr[2] = { p1, p2 };
+	vector<double> p0 = arr[n-1];				// p0 = pn
 
-	while (i <= n)
+	vector<vector<double>> y = { {8, 9} };
+	y.push_back({ 0,0 });
+	y.push_back({ 0,0 });
+	y.push_back({ 0,0 });
+
+	while (k < N)
 	{
-		if (y0 == NULL && y1 == NULL)
+		while (i <= n)
 		{
-			y0 = x0; y1 = x1;
+
+			y[i + 1][0] = y[i][0] + Optim(y[i][0], y[i][1], arr[0]);
+			y[i + 1][1] = y[i][1] + Optim(y[i][0], y[i][1], arr[1]);
+			cout << "Y[0] = " << y[0][0] << y[0][1] << endl;
+			cout << "Y = " << y[i][0] << y[i][1] << endl;
+			cout << "Y[n] = " << y[n][0] << y[n][1] << endl;
+			cout << "i = " << i << endl << endl;
+			if (i == n - 1 && y[n] == y[0])
+			{
+				cout << "X = {" << y[n][0] << " ; " << y[n][1] << " } " << endl;
+				return 0;
+			}
+			if (i == n && y[n + 1] == y[1])
+			{
+				cout << "X = {" << y[n + 1][0] << " ; " << y[n + 1][1] << " } " << endl;
+				return 0;
+			}
+			i++;
 		}
-		if (!(i % 2))
+		x.push_back(y[n + 1]);
+		if (sqrt(pow(x[k + 1][0] - x[k][0], 2) + pow(x[k + 1][1] - x[k][1], 2)) < EPS)
 		{
-			y1 = y1 + Optim(y0, y1, arr[1]);
+			cout << "X = {" << x[k + 1][0] << " ; " << x[k + 1][1] << " } " << endl;
+			return 0;
 		}
 		else
 		{
-			y0 = y0 + Optim(y0, y1, arr[0]);
+			arr[0] = arr[1];
+			arr[1] = { y[n + 1][0] - y[1][0], y[n + 1][1] - y[1][1] };
+			y[0] = x[k + 1];
+			k++; i = 0;
 		}
-		i++;
 	}
-	cout << y0 << y1;
 }
 
 int main()
 {
 	Directions();
-    return 0;
+	return 0;
 }
-
